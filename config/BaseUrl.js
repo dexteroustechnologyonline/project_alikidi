f0fa1bdf08afd01c6659c9edcb3fd3a537e8c3e7
Nalla Manoj Kumar <100987446+manojnalla098@users.noreply.github.com>
2023-09-26 16:37:30 +0530

    26-09-23 nesw

--- config/BaseUrl.js
+++ config/BaseUrl.js
@@ -1,5 +1,5 @@
 // export const Baseurl = "http://localhost:4004";
 // export const Baseurl = "https://saadhyam-api.onrender.com";
 export const Baseurl = "https://alikidi.dexteroustechnology.in";
-
+export const Baseurl2 = "https://saadhyam-api.onrender.com";
 
